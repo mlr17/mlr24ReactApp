5f14408c7b6b5a1c0f3dd73fe28d2607f3b9f664
mlr17 <mlr8669sdu@163.com>
2023-07-18 22:54:09 +0800

    第二次提交

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
+import React from 'react';
+
 export default function Main() {
+    //第二次修改
+    const datas = [
+        {title:'mlr',age:25},
+        {title:'hr',age:24},
+        {title:'hk',age:10}
+    ]
+    const renderEle = (datas)=>{
+        return datas.map((data)=>{
+            return (
+                <li key={data.title} >{`${data.title}今年${data.age}了`}</li>
+            )
+        })
+    }
     return (
         <main>
             <article>
                 <h2>Welcome to my home page!</h2>
                 <p>This is a demo page for the CSS preprocessor LESS.</p>
             </article>
+            <ul>{renderEle(datas)}</ul>
         </main>
     )
 }
