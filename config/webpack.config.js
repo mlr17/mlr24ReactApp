d126ee311bf39ad519c038cbc2c081a4f0675830
mlr17 <mlr8669sdu@163.com>
2023-07-18 21:54:21 +0800

    第一次提交

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -547,12 +547,11 @@ module.exports = function (webpackEnv) {
             },
             {
               test: /\.less$/,
-                use: getStyleLoaders(
-                  {
-                    //暂不配置
-                  },
+                use:[
+                  'style-loader',
+                  'css-loader',
                   'less-loader'
-                ),
+                ]
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import "./assets/base.less"
 import Router from './router'
-
-
-ReactDOM.render(
-    <Router />,
-    document.getElementById('root')
+//import App from './App'
+const rootDiv = ReactDOM.createRoot(document.getElementById('root'))
+rootDiv.render(
+    <Router />
 )
